c1b6f3af5ea7b40a138f3bcd15f79ef7d5dc6de2
naufal <naufal@legendarymemory.com>
2018-11-26 22:14:06 +0800

    Add comment for future developer to understand the code

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 )
 
+// Struct for all the entities
 type producer struct {
 	id string
 }
@@ -25,11 +26,13 @@ type widget struct {
 	broken bool
 }
 
+// Example - go run main.go -n 10 -p 1 -c 1 -k 3
 var widgetFlag int
 var producerFlag int
 var consumerFlag int
 var widgetBrokenFlag int
 
+// Randomly choose true or false and if true minus the widget flag
 func (w *widget) isWidgetBroken(widgetFlag int) bool {
 	broken := mathRandom.Int()%2 == 0
 	if broken && (widgetFlag > 0) {
@@ -39,6 +42,10 @@ func (w *widget) isWidgetBroken(widgetFlag int) bool {
 	return false
 }
 
+/*
+	1. Generate Unique universal ID for each widget
+	2. Example UUID - 91f023ae62e42b44-af6103c770dc1dda
+*/
 func (w *widget) generateUUID() {
 	b := make([]byte, 16)
 	_, err := rand.Read(b)
@@ -51,17 +58,20 @@ func (w *widget) generateUUID() {
 	w.id = uuid
 }
 
+// create a new producer object
 func hireProducer(number int) producer {
 	id := fmt.Sprintf("producer_%d", number)
 	p := producer{id: id}
 	return p
 }
 
+// As per the requirement, the producer has to set the time
 func (p *producer) setTime() time.Time {
 	currentTime := time.Now()
 	return currentTime
 }
 
+// create widget function by each producer
 func (p *producer) createWidget() widget {
 	w := widget{}
 	w.generateUUID()
@@ -71,30 +81,11 @@ func (p *producer) createWidget() widget {
 	return w
 }
 
-func consumerEnterTheShop(number int) consumer {
-	id := fmt.Sprintf("consumer_%d", number)
-	c := consumer{id: id}
-	return c
-}
-
-func (c *consumer) consumeTime(w time.Time) time.Duration {
-	consumeTime := time.Now().Sub(w)
-	return consumeTime
-}
-
-func (c *consumer) consumeWidget(w widget) {
-	if w.broken {
-		fmt.Println(c.id, "consumes [id=", w.id, "source=", w.source,
-			"time=", w.time.Format("15:04:05.999999"), "broken=", w.broken,
-			"-- stopping production [execution stops]")
-		os.Exit(0)
-	} else {
-		fmt.Println(c.id, "consumes [id=", w.id, "source=", w.source,
-			"time=", w.time.Format("15:04:05.999999"), "broken=", w.broken,
-			"in", c.consumeTime(w.time), "time")
-	}
-}
-
+/*
+	The main function that will hire the producer and
+	assign the developer to the respective widget creation
+	Using channel to communicate with the shop channel once done
+*/
 func startProduction(id int, production <-chan int, shop chan<- widget) {
 	for range production {
 		p := hireProducer(id)
@@ -108,6 +99,43 @@ func startProduction(id int, production <-chan int, shop chan<- widget) {
 	}
 }
 
+// create a new consumer object
+func consumerEnterTheShop(number int) consumer {
+	id := fmt.Sprintf("consumer_%d", number)
+	c := consumer{id: id}
+	return c
+}
+
+/*
+	When consumer consume the widget take the
+	(consume time - widget creation time)
+*/
+func (c *consumer) consumeTime(w time.Time) time.Duration {
+	consumeTime := time.Now().Sub(w)
+	return consumeTime
+}
+
+/*
+	This is when the message will show up depending on whether
+	the widget is broken or not
+*/
+func (c *consumer) consumeWidget(w widget) {
+	if w.broken {
+		fmt.Println(c.id, "consumes [id=", w.id, "source=", w.source,
+			"time=", w.time.Format("15:04:05.999999"), "broken=", w.broken,
+			"-- stopping production [execution stops]")
+		os.Exit(0)
+	} else {
+		fmt.Println(c.id, "consumes [id=", w.id, "source=", w.source,
+			"time=", w.time.Format("15:04:05.999999"), "broken=", w.broken,
+			"in", c.consumeTime(w.time), "time")
+	}
+}
+
+/*
+	This is the second main function.
+	If widget has been created then shop channe will listen
+*/
 func consumerBuying(id int, shop <-chan widget, done chan<- widget) {
 	for w := range shop {
 		consumer := consumerEnterTheShop(id)
@@ -118,25 +146,35 @@ func consumerBuying(id int, shop <-chan widget, done chan<- widget) {
 
 func main() {
 
+	// Taking all the flag
 	flag.IntVar(&widgetFlag, "n", 0, "an int")
 	flag.IntVar(&producerFlag, "p", 0, "an int")
 	flag.IntVar(&consumerFlag, "c", 0, "an int")
 	flag.IntVar(&widgetBrokenFlag, "k", 0, "an int")
 	flag.Parse()
 
+	/* All the main channels */
 	production := make(chan int, 40)
 	shop := make(chan widget, 40)
 	done := make(chan widget, 40)
 
+	/*
+		Loop the producer flag and then assign the producer
+		to create the widget
+	*/
 	for s := 1; s <= producerFlag; s++ {
 		go startProduction(s, production, shop)
 	}
 
-	// Send to production channel
+	/* Send to production channel */
 	for p := 1; p <= widgetFlag; p++ {
 		production <- p
 	}
 
+	/*
+		Loop the consumer flag and then assign the consumer
+		to consume the widget
+	*/
 	for c := 1; c <= consumerFlag; c++ {
 		go consumerBuying(c, shop, done)
 	}
